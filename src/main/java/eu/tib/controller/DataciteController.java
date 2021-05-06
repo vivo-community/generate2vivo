c90f22fb013eb8b80aac5515a6bd2b9385f3ff20
Sandra Mierz <sandra.mierz@tib.eu>
2021-05-06 18:47:45 +0200

    refactor using config file

--- src/main/java/eu/tib/controller/DataciteController.java
+++ src/main/java/eu/tib/controller/DataciteController.java
@@ -22,21 +22,40 @@ import java.util.Collections;
 @Slf4j
 @Validated
 @RestController
-@RequestMapping(value = "/person")
-@Api(value = "Controller", tags = {"person"})
-public class PersonController {
+@RequestMapping(value = "/datacitecommons")
+@Api(value = "Controller", tags = {"datacitecommons"})
+public class DataciteController {
 
     @Autowired
     private ResponseService responseService;
 
-    @PostMapping(produces = "application/json")
+    @PostMapping(value = "/getOrganizationPlusPersons", produces = "application/json")
+    public ResponseEntity<String> getOrganizationPlusPersons(
+            @Valid @Pattern(regexp = "^https://ror.org/\\d{2}[a-z0-9]{5}\\d{2}")
+            @ApiParam("Complete ROR URL consisting of https://ror.org/ plus id")
+            @RequestParam String ror)
+            throws SparqlParsingException, SparqlExecutionException {
+
+        final String id = "/sparql/datacitecommons/orga2person";
+        log.info("Incoming Request for " + id + " with ror: " + ror);
+        StopWatch stopWatch = new StopWatch(id);
+        stopWatch.start(id);
+
+        ResponseEntity result = responseService.buildResponse(id, Collections.singletonMap("ror", ror));
+
+        stopWatch.stop();
+        log.info(id + " took " + stopWatch.getTotalTimeSeconds() + "s");
+        return result;
+    }
+
+    @PostMapping(value = "/getPersonPlusPublications", produces = "application/json")
     public ResponseEntity<String> getPersonPlusPublications(
             @Valid @Pattern(regexp = "^https://orcid.org/\\d{4}-\\d{4}-\\d{4}-\\d{4}")
             @ApiParam("Complete Orcid URL consisting of https://orcid.org/ plus id")
             @RequestParam String orcid)
             throws SparqlParsingException, SparqlExecutionException {
 
-        final String id = "orcid/person2publication";
+        final String id = "/sparql/datacitecommons/person2publication";
         log.info("Incoming Request for " + id + " with orcid: " + orcid);
         StopWatch stopWatch = new StopWatch(id);
         stopWatch.start(id);
--- src/main/java/eu/tib/service/GeneratePipeline.java
+++ src/main/java/eu/tib/service/GeneratePipeline.java
@@ -1,9 +1,9 @@
 package eu.tib.service;
 
-import com.lambdista.util.Try;
-import eu.tib.error.SparqlExecutionException;
-import eu.tib.error.SparqlParsingException;
-import eu.tib.utils.FileUtils;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import eu.tib.utils.ResourceUtils;
+import fr.mines_stetienne.ci.sparql_generate.FileConfigurations;
 import fr.mines_stetienne.ci.sparql_generate.SPARQLExt;
 import fr.mines_stetienne.ci.sparql_generate.engine.PlanFactory;
 import fr.mines_stetienne.ci.sparql_generate.engine.RootPlan;
@@ -13,11 +13,15 @@ import fr.mines_stetienne.ci.sparql_generate.stream.LocatorFileAccept;
 import fr.mines_stetienne.ci.sparql_generate.stream.SPARQLExtStreamManager;
 import fr.mines_stetienne.ci.sparql_generate.utils.ContextUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.DatasetFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolutionMap;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.sparql.engine.binding.Binding;
 import org.apache.jena.sparql.engine.binding.BindingUtils;
 import org.apache.jena.sparql.util.Context;
@@ -31,66 +35,151 @@ import java.util.Map;
 @Slf4j
 public class GeneratePipeline {
 
-    public Model run(String queryName, Map<String, String> input) {
+    private static final String CONF_FILE = "sparql-generate-conf.json";
 
-        log.info("Read-in Generate Query for " + queryName);
-        Try<SPARQLExtQuery> generateQuery = Try.apply(() -> parseSparqlGenerateQuery(queryName));
-        generateQuery.failed().forEach(e -> {
-            log.error("error while parsing Sparql Query: ",e);
-            throw new SparqlParsingException(GeneratePipeline.class, queryName, input.toString());
-        });
-        log.info("Finished reading Generate Query");
+    public Model run(String confPath, Map<String, String> input) {
+        log.info("Read config for " + confPath);
+        FileConfigurations config = readConfig(confPath + File.separator + CONF_FILE);
 
-        log.info("Run generate query");
-        Try<Model> result = generateQuery.flatMap(generate ->
-                Try.apply(() -> executeGenerate(input, generate)));
-        result.failed().forEach(e -> {
-            log.error("error while executing Sparql Query: ",e);
-            throw new SparqlExecutionException(GeneratePipeline.class, queryName, input.toString());
-        });
-        log.info("Finished running generate query");
+        log.info("Read-in Generate Query for " + config.query);
+        String queryPath = confPath + File.separator + config.query;
+        SPARQLExtQuery query = parseSparqlGenerateQuery(queryPath, config.base);
 
-        return result.get();
-    }
+        log.info("Initialize stream manager");
+        SPARQLExtStreamManager sm = null;
+        try {
+            sm = prepareStreamManager(confPath, config);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    public SPARQLExtQuery parseSparqlGenerateQuery(String queryName) throws IOException {
-        // Parse sparql-generate query
-        String sparqlGenerateString = new FileUtils().getSparqlQuery(queryName);
-        SPARQLExtQuery query = (SPARQLExtQuery) QueryFactory
-                .create(sparqlGenerateString, SPARQLExt.SYNTAX);
-        return query;
-    }
-
-    public Model executeGenerate(Map<String, String> input, SPARQLExtQuery query) {
-        // replace source in sparql-generate (default.json) with GraphQL URI
-        LocatorFileAccept locator = new LocatorFileAccept(new File("resources").toURI().getPath());
-        LocationMapperAccept mapper = new LocationMapperAccept();
-        mapper.addAltEntry("https://projects.tib.eu/tapir/graphql/orga2person.graphql", "graphql/ror/orga2person.graphql");
-        mapper.addAltEntry("https://projects.tib.eu/tapir/graphql/orga2person.rqg", "sparql/ror/orga2person.rqg");
-
-        mapper.addAltEntry("https://projects.tib.eu/tapir/graphql/person2publication.graphql", "graphql/orcid/person2publication.graphql");
-        mapper.addAltEntry("https://projects.tib.eu/tapir/graphql/person2publication.rqg", "sparql/orcid/person2publication.rqg");
-        SPARQLExtStreamManager sm = SPARQLExtStreamManager.makeStreamManager(locator, mapper);
-
-        // create the context
-        Model model = ModelFactory.createDefaultModel();
+        log.info("Create context");
+        Dataset ds = getDataset(confPath, config);
         Context context = ContextUtils.build()
-                .setInputModel(model)
+                .setBase(config.base)
+                .setPrefixMapping(query)
+                .setInputDataset(ds)
                 .setStreamManager(sm)
                 .build();
 
-        // transfer initial ror parameter into query via binding
+        List<Binding> bindings = input2Bindings(input);
+
+        log.info("Create & execute plan");
+        RootPlan plan = PlanFactory.create(query);
+        Model output = plan.execGenerate(bindings, context);
+        log.info("Finished executing query");
+
+        return output;
+    }
+
+    public FileConfigurations readConfig(String resPath) {
+        try {
+            String conf = new ResourceUtils().readResource(resPath);
+            FileConfigurations config = (new Gson()).fromJson(conf, FileConfigurations.class);
+            return config;
+        } catch (IOException ex) {
+            log.warn("IOException while reading the config file.", ex);
+        } catch (JsonSyntaxException ex) {
+            log.warn("JSON Syntax exception while loading the location mapping model for the queryset.", ex);
+        }
+
+        throw new RuntimeException("Error while reading config.");
+    }
+
+    public SPARQLExtQuery parseSparqlGenerateQuery(String queryName, String base) {
+        try {
+            String sparqlGenerateString = new ResourceUtils().readResource(queryName);
+            SPARQLExtQuery query = (SPARQLExtQuery) QueryFactory.create(sparqlGenerateString, base, SPARQLExt.SYNTAX);
+            if (!query.explicitlySetBaseURI()) query.setBaseURI(base);
+            return query;
+        } catch (IOException | NullPointerException ex) {
+            log.error(String.format("No query file named %s was found.", queryName), ex);
+        }
+
+        throw new RuntimeException("Error while parsing the query to be executed.");
+    }
+
+    public Dataset getDataset(String confPath, FileConfigurations config) {
+        Dataset ds = DatasetFactory.create();
+
+        if (config.graph != null) {
+            try {
+                File dsFromResource = new ResourceUtils().resource2File(confPath + File.separator + config.graph);
+                ds.setDefaultModel(RDFDataMgr.loadModel(dsFromResource.toString(), Lang.TTL));
+            } catch (Exception ex) {
+                log.debug("Graph could not be loaded: " + ex.getMessage());
+            }
+        }
+
+        if (config.namedgraphs != null) {
+            config.namedgraphs.forEach((ng) -> {
+                try {
+                    File ngFromResource = new ResourceUtils().resource2File(confPath + File.separator + ng.path);
+                    Model model = RDFDataMgr.loadModel(ngFromResource.toString(), Lang.TTL);
+                    ds.addNamedModel(ng.uri, model);
+                } catch (Exception ex) {
+                    log.debug("Cannot load named graph " + ng.path + ": " + ex.getMessage());
+                }
+            });
+        }
+        return ds;
+    }
+
+    public SPARQLExtStreamManager prepareStreamManager(String confPath, FileConfigurations config) throws
+            IOException {
+        ResourceUtils futils = new ResourceUtils();
+        String resPath = System.getProperty("java.io.tmpdir");
+
+        LocatorFileAccept locator = new LocatorFileAccept(resPath);
+        LocationMapperAccept mapper = new LocationMapperAccept();
+        SPARQLExtStreamManager sm = SPARQLExtStreamManager.makeStreamManager(locator);
+        sm.setLocationMapper(mapper);
+
+        if (config.namedqueries != null) {
+            log.debug("Mapping namedqueries..");
+            config.namedqueries.forEach((doc) -> {
+                try {
+                    File nqAsResource = futils.resource2File(confPath + File.separator + doc.path);
+                    mapper.addAltEntry(doc.uri, nqAsResource.getAbsolutePath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        if (config.documentset != null) {
+            log.debug("Mapping documentset..");
+            config.documentset.forEach((doc) -> {
+                try {
+                    File dsAsResource = futils.resource2File(confPath + File.separator + doc.path);
+                    mapper.addAltEntry(doc.uri, dsAsResource.getAbsolutePath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        if (config.namedgraphs != null) {
+            log.debug("Mapping namedgraphs..");
+            config.namedgraphs.forEach((doc) -> {
+                try {
+                    File ngAsResource = futils.resource2File(confPath + File.separator + doc.path);
+                    mapper.addAltEntry(doc.uri, ngAsResource.getAbsolutePath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        return sm;
+    }
+
+    public List<Binding> input2Bindings(Map<String, String> input) {
+        // transfer input parameters into query via binding
         QuerySolutionMap initialBinding = new QuerySolutionMap();
+        Model model = ModelFactory.createDefaultModel();
         input.forEach((k, v) -> {
             RDFNode literal = model.createLiteral(v);
             initialBinding.add(k, literal);
         });
-        List<Binding> bindings = Collections.singletonList(BindingUtils.asBinding(initialBinding));
-
-        // create & execute the plan
-        RootPlan plan = PlanFactory.create(query);
-        Model output = plan.execGenerate(bindings, context);
-
-        return output;
+        return Collections.singletonList(BindingUtils.asBinding(initialBinding));
     }
 }
