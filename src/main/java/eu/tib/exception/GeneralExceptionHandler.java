649e231b57f182606e577e94de3328d51b359838
Sandra Mierz <sandra.mierz@tib.eu>
2021-06-18 15:36:36 +0200

    rm unnecessary imports

--- src/main/java/eu/tib/exception/GeneralExceptionHandler.java
+++ src/main/java/eu/tib/exception/GeneralExceptionHandler.java
@@ -4,17 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.validation.FieldError;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
--- src/main/java/eu/tib/exception/VIVOExportException.java
+++ src/main/java/eu/tib/exception/VIVOExportException.java
@@ -1,7 +1,6 @@
 package eu.tib.exception;
 
 public class VIVOExportException extends RuntimeException {
-
     public VIVOExportException(String message) {
         super(message);
     }
